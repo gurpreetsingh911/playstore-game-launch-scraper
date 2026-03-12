4741dc4f501e4385e481146fb154989c1d42a2d6
Gurpreet Singh <gpswazir@gmail.com>
2026-03-12 19:22:56 +0530

    Added backend sucess message to frontend

--- myapp/public/script.js
+++ myapp/public/script.js
@@ -12,10 +12,36 @@ async function fetchGame() {
 
     const data = await response.json();
 
+    document.getElementById("message").innerHTML = `
+      Status: ${data.message} <br>
+    
+    `;
+        
+    
     document.getElementById("result").innerHTML = `
       <h3>Result</h3>
       Game Name: ${data.gameName} <br>
       Developer: ${data.developer} <br>
       Play Link: ${data.googleLink}
   `;
+}
+
+
+async function addGame() {
+
+  const url = document.getElementById("gameUrl").value;
+
+  const response = await fetch("/fetch-game", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ url })
+  });
+
+  const data = await response.json();
+
+  document.getElementById("gameName").innerText = data.gameName;
+  document.getElementById("developer").innerText = data.developer;
+  document.getElementById("playLink").innerText = data.googleLink;
 }
--- myapp/server.js
+++ myapp/server.js
@@ -30,7 +30,7 @@ app.listen(PORT, () => {
 // Fetch Game Route
 app.post("/fetch-game", async (req, res) => {
 
-   let browser;
+  let browser;
 
   try {
 
@@ -52,151 +52,151 @@ app.post("/fetch-game", async (req, res) => {
       });
     }
 
-      // Validate URL format
-      try {
-        new URL(url);
-      } catch {
-        return res.status(400).json({
-          error: "Invalid URL format"
-        });
-      }
-
-      // Check if it is Play Store link
-      if (!url.includes("play.google.com")) {
-        return res.status(400).json({
-          error: "Please provide a valid Google Play Store URL"
-        });
-      }
-
-      // If valid → continue with Playwright
-
-       browser = await chromium.launch({
-        headless: false,
+    // Validate URL format
+    try {
+      new URL(url);
+    } catch {
+      return res.status(400).json({
+        error: "Invalid URL format"
       });
-   
-      const page = await browser.newPage();
+    }
 
-        // Open Play Store page
-      await page.goto(url, { waitUntil: "domcontentloaded" });
+    // Check if it is Play Store link
+    if (!url.includes("play.google.com")) {
+      return res.status(400).json({
+        error: "Please provide a valid Google Play Store URL"
+      });
+    }
 
-      await page.waitForLoadState('networkidle');
+    // If valid → continue with Playwright
 
-      // Get Game Name
-      const gameTitle = await page.locator("h1").first().textContent();
+    browser = await chromium.launch({
+      headless: false,
+    });
 
-      // Get Google Play link
-      const Google_link = page.url();
+    const page = await browser.newPage();
 
-      // Get Developer name
-      const developer = await page.locator('a[href*="dev"]').first().textContent();
+    // Open Play Store page
+    await page.goto(url, { waitUntil: "domcontentloaded" });
 
-      // fetch Dev id & Locate by text (e.g., the "Website" link on a Play Store page)
+    await page.waitForLoadState('networkidle');
 
-      let dev_href = "N/A";
+    // Get Game Name
+    const gameTitle = await page.locator("h1").first().textContent();
 
-      try {
+    // Get Google Play link
+    const Google_link = page.url();
 
-        // get developer page link
-        const devPage = await page.locator('a[href*="/store/apps/dev"]').first().getAttribute("href");
+    // Get Developer name
+    const developer = await page.locator('a[href*="dev"]').first().textContent();
 
-        const devUrl = "https://play.google.com" + devPage;
+    // fetch Dev id & Locate by text (e.g., the "Website" link on a Play Store page)
 
-        console.log("Developer Page:", devUrl);
+    let dev_href = "N/A";
 
-        // open developer page
-        await page.goto(devUrl, { waitUntil: "domcontentloaded" });
+    try {
 
-        const websiteLocator = page.getByRole("link", { name: /visit website/i });
+      // get developer page link
+      const devPage = await page.locator('a[href*="/store/apps/dev"]').first().getAttribute("href");
 
-        if (await websiteLocator.count() > 0) {
-          dev_href = await websiteLocator.first().getAttribute("href");
-        }
+      const devUrl = "https://play.google.com" + devPage;
 
-      } catch (err) {
-        dev_href = "N/A";
+      console.log("Developer Page:", devUrl);
+
+      // open developer page
+      await page.goto(devUrl, { waitUntil: "domcontentloaded" });
+
+      const websiteLocator = page.getByRole("link", { name: /visit website/i });
+
+      if (await websiteLocator.count() > 0) {
+        dev_href = await websiteLocator.first().getAttribute("href");
       }
-     
-      // 1. opeaning google form
 
-      const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfNFbqE1XvEbd3xZlkXAhKmrkzH2OxZXuYcaK7d61CcAztSEg/viewform";
-      await page.goto(formUrl);
+    } catch (err) {
+      dev_href = "N/A";
+    }
 
-      //2. Check Email field
-      const Email_Check_Box = await page.getByRole('checkbox', { name: 'Record email as the email to' });
+    // 1. opeaning google form
 
-      //2.1 Perform an action, e.g., click or fill
-      await Email_Check_Box.click();
+    const formUrl = "https://docs.google.com/forms/d/e/1FAIpQLSfNFbqE1XvEbd3xZlkXAhKmrkzH2OxZXuYcaK7d61CcAztSEg/viewform";
+    await page.goto(formUrl);
 
-      // 3. Add Google Form URL
-      await page.getByLabel('Game Name').fill(gameTitle);
+    //2. Check Email field
+    const Email_Check_Box = await page.getByRole('checkbox', { name: 'Record email as the email to' });
 
-      // 4. Add Google Play Link
-      await page.getByLabel('Google play link').fill(Google_link);
+    //2.1 Perform an action, e.g., click or fill
+    await Email_Check_Box.click();
 
-      // 5. Developer Name
-      await page.getByLabel('Developer name (must required for the New Launch)').fill(developer);
+    // 3. Add Google Form URL
+    await page.getByLabel('Game Name').fill(gameTitle);
 
-      //------------------STEP 2 — Fill Text Fields-----------------
+    // 4. Add Google Play Link
+    await page.getByLabel('Google play link').fill(Google_link);
 
-      // 1. Click on Game submitted for -> Game launch
-      await page.getByText('Game launch', { exact: true }).click();
+    // 5. Developer Name
+    await page.getByLabel('Developer name (must required for the New Launch)').fill(developer);
 
-      // 2. Click Any kind of deal expected? (ONLY for new launches) -> Yes
-      await page.getByLabel('Any kind of deal expected? ').getByText('CPI', { exact: true }).click();
+    //------------------STEP 2 — Fill Text Fields-----------------
 
-      // 3. Launch timeframe -> add todays data
-      //For added Current date in Launch timeframe
+    // 1. Click on Game submitted for -> Game launch
+    await page.getByText('Game launch', { exact: true }).click();
 
-      let Launch_Timeframe = await page.getByLabel(`Launch timeframe`);
+    // 2. Click Any kind of deal expected? (ONLY for new launches) -> Yes
+    await page.getByLabel('Any kind of deal expected? ').getByText('CPI', { exact: true }).click();
 
-      let CurrentDate = new Date();
+    // 3. Launch timeframe -> add todays data
+    //For added Current date in Launch timeframe
 
-      let today_day = CurrentDate.getDate();
-      let today_Month = CurrentDate.getMonth() + 1; // months start from 0
-      let today_Year = CurrentDate.getFullYear();
+    let Launch_Timeframe = await page.getByLabel(`Launch timeframe`);
 
-      let updateDate = (`${today_day}-${today_Month}-${today_Year}`);
+    let CurrentDate = new Date();
 
-      await Launch_Timeframe.click();
-      await Launch_Timeframe.fill(updateDate);
+    let today_day = CurrentDate.getDate();
+    let today_Month = CurrentDate.getMonth() + 1; // months start from 0
+    let today_Year = CurrentDate.getFullYear();
 
-      // await Launch_Timeframe.nth(0).fill(`${today_day}`);
-      // await Launch_Timeframe.nth(1).fill(`${today_Month}`);
-      // await Launch_Timeframe.nth(2).fill(`${today_Year}`);
+    let updateDate = (`${today_day}-${today_Month}-${today_Year}`);
 
+    await Launch_Timeframe.click();
+    await Launch_Timeframe.fill(updateDate);
 
-      // 4. Fill Target geo?
-
-      const language = await detectLanguage(gameTitle);
-      //console.log("Detected language:", language);
-
-      await page.getByLabel('Target geo?').fill(language);
-
-      //await page.waitForTimeout(5000); // waits for 2 seconds
-
-      //STEP 5 — Fill Expected Rating
-      await page.getByLabel('Expected game rating/Tier?').fill('IP2');
-
-      //STEP 6 — Check mark Live in other geo or not
-
-      await page.getByLabel('ONLY for new launches').getByText('Yes', { exact: true }).click();
+    // await Launch_Timeframe.nth(0).fill(`${today_day}`);
+    // await Launch_Timeframe.nth(1).fill(`${today_Month}`);
+    // await Launch_Timeframe.nth(2).fill(`${today_Year}`);
 
 
-      //STEP 7 — fill the Fetch dev "Dev_href" website link (if available)
+    // 4. Fill Target geo?
 
-      await page.getByLabel('Official forums (game website, FB').fill(dev_href);
+    const language = await detectLanguage(gameTitle);
+    //console.log("Detected language:", language);
 
-      // Step click on submit button
+    await page.getByLabel('Target geo?').fill(language);
+
+    //await page.waitForTimeout(5000); // waits for 2 seconds
+
+    //STEP 5 — Fill Expected Rating
+    await page.getByLabel('Expected game rating/Tier?').fill('IP2');
+
+    //STEP 6 — Check mark Live in other geo or not
+
+    await page.getByLabel('ONLY for new launches').getByText('Yes', { exact: true }).click();
 
 
-      await page.getByRole('button', { name: 'submit' }).click();
+    //STEP 7 — fill the Fetch dev "Dev_href" website link (if available)
 
-      await page.waitForTimeout(2000);
+    await page.getByLabel('Official forums (game website, FB').fill(dev_href);
 
-       // Close browser
-         await browser.close();
+    // Step click on submit button
 
-     //  Optional form submission via API
+
+    await page.getByRole('button', { name: 'submit' }).click();
+
+    await page.waitForTimeout(2000);
+
+    // Close browser
+    await browser.close();
+
+    //  Optional form submission via API
     await axios.post(
       "https://docs.google.com/forms/d/e/1FAIpQLSefoeEJUV7joqOQGsHFFf4ukw5NhtnFZYZVwjnUzg9Kpsqcig/formResponse",
       new URLSearchParams({
@@ -212,7 +212,7 @@ app.post("/fetch-game", async (req, res) => {
       }
     );
 
-   
+
 
     // API response
     res.json({
@@ -221,7 +221,9 @@ app.post("/fetch-game", async (req, res) => {
       googleLink: Google_link,
       developer,
       developerWebsite: dev_href,
-      targetGeo: language
+      targetGeo: language,
+      status: "success",
+      message: "Game added successfully and submitted to Google Form"
     });
 
   } catch (error) {
